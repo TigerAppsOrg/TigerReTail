46ee54305a8312db9406ce67e372f33bfdd7ad31
shannon-heh <shannon.heh@princeton.edu>
2022-11-19 18:52:21 -0500

    remove email notice for expiration

--- TRT-django/marketplace/management/commands/delete_expired.py
+++ TRT-django/marketplace/management/commands/delete_expired.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from marketplace.models import (Item, ItemRequest)
+from marketplace.models import Item, ItemRequest
 from django.utils import timezone
 from django.conf import settings
 
@@ -7,6 +7,7 @@ class Command(BaseCommand):
     help = 'Delete expired items'
 
     def handle(self, *args, **options):
+        print("Running delete expired task")
         self.deleteExpired()
 
     def deleteExpired(self):
@@ -14,26 +15,8 @@ class Command(BaseCommand):
             deadline__lt=timezone.now() - settings.EXPIRATION_BUFFER,
             status = Item.AVAILABLE
         )
-        print("Running delete expired task")
         print(f"Number of expired items: {len(expired_items)}")
         for item in expired_items:
-            pass
-            # send email notice
-            # send_mail_activity(
-            #     "Expired Item Removed",
-            #     "Your expired item '"
-            #     + item.name
-            #     + "' has been removed.\nIf you would still like to sell your item, please feel free to relist it.",
-            #     settings.EMAIL_NAME,
-            #     [item.seller],
-            #     fail_silently=True,
-            # )
-            # # notify the seller
-            # notify(
-            #     item.seller,
-            #     "Your expired item '" + item.name + "' has been removed",
-            #     reverse("list_items"),
-            # )
             # delete the item
             item.delete()
 
@@ -42,23 +25,6 @@ class Command(BaseCommand):
         )
         print(f"Number of expired requests: {len(expired_item_requests)}")
         for item_request in expired_item_requests:
-            pass
-            # send email notice
-            # send_mail_activity(
-            #     "Expired Item Request Removed",
-            #     "Your expired item request '"
-            #     + item_request.name
-            #     + "' has been removed.\nIf you would still like to request the item, please feel free to make another request.",
-            #     settings.EMAIL_NAME,
-            #     [item_request.requester],
-            #     fail_silently=True,
-            # )
-            # # notify the requester
-            # notify(
-            #     item_request.requester,
-            #     "Your expired item request '" + item_request.name + "' has been removed",
-            #     reverse("list_item_requests"),
-            # )
             # delete the item request
             item_request.delete()
 
